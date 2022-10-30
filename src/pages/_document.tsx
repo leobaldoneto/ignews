149462f2d18ee380d0e38e316035a70e97a660a2
Leobaldo Alcantara Neto <leobaldoneto@gmail.com>
2022-10-30 17:22:06 -0300

    Changed title tag location.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,6 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com"/>
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap" rel="stylesheet"/>
-        <title>ig.news</title>
       </Head>
       <body>
         <Main />
