153271a841bbeb63217ddedb50625be19c321495
Leobaldo Alcantara Neto <leobaldoneto@gmail.com>
2022-10-31 10:29:35 -0300

    Changed <a> to <Link>.

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import styles from './styles.module.scss'
 import logo from '../../../public/images/logo.svg'
 import { SignInButton } from '../SignInButton'
+import Link from 'next/link'
 
 export function Header() {
   return (
@@ -9,8 +10,8 @@ export function Header() {
       <div className={styles.headerContent}>
         <Image src={logo} alt="ig.news"/>
         <nav>
-          <a href='#'>Home</a>
-          <a href='#'>Posts</a>
+          <Link href='/'>Home</Link>
+          <Link href='/posts'>Posts</Link>
         </nav>
         <SignInButton/>
       </div>
