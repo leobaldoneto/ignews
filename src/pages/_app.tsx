7c493de548edea7e06d3e5d72b406e1cde1b3a00
Leobaldo Alcantara Neto <leobaldoneto@gmail.com>
2022-11-01 09:11:28 -0300

    Feat: GitHub auth with next-auth.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,15 @@
 import type { AppProps } from 'next/app'
 import { Header } from '../components/header';
+import { SessionProvider } from 'next-auth/react'
 
 import '../styles/global.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <SessionProvider session={pageProps.session}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </SessionProvider>
   )
 }
 
