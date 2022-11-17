de5f19da05ad62c600874d29d502f665d02ab60a
Leobaldo Alcantara Neto <leobaldoneto@gmail.com>
2022-11-17 00:20:59 -0300

    Header component renaming.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from 'next/app'
 
-import { Header } from '../components/header'
+import { Header } from '../components/Header'
 
 import { SessionProvider } from 'next-auth/react'
 import type { Session } from 'next-auth'
