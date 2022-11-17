c3481b466ecc914fe29deb5c7ec114f2dce83d37
Leobaldo Alcantara Neto <leobaldoneto@gmail.com>
2022-11-17 00:16:55 -0300

    Feat: header menu activa link control.

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -3,15 +3,19 @@ import styles from './styles.module.scss'
 import logo from '../../../public/images/logo.svg'
 import { SignInButton } from '../SignInButton'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 export function Header() {
+  const { asPath } = useRouter();
+  console.log(asPath);
+
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <Image src={logo} alt="ig.news"/>
         <nav>
-          <Link href='/'>Home</Link>
-          <Link href='/posts'>Posts</Link>
+          <Link href='/' className={asPath === '/' ? styles.active : ''}>Home</Link>
+          <Link href='/posts' className={asPath === '/posts' ? styles.active : ''}>Posts</Link>
         </nav>
         <SignInButton/>
       </div>
